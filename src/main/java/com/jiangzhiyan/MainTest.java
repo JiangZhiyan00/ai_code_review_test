7b8f6abcc5d22477182c83821323496589e4a914
JiangZhiyan <jaemusic@foxmail.com>
2024-07-16 20:04:45 +0800

    Update MainTest.java

--- src/main/java/com/jiangzhiyan/MainTest.java
+++ src/main/java/com/jiangzhiyan/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
     public static void main(String[] args) {
         //循环打印文本
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello World! " + i);
+            System.out.println("Hello World!!! " + i);
         }
     }
 }
